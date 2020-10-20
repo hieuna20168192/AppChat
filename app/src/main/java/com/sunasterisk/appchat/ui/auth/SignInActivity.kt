8ec513726c412fc87b5e86b449dc73e0db3ee1e5
hieuna20168192 <nguyenanhhieu231298@gmail.com>
2020-10-20 21:35:00 +0700

    Refs [Task][30731] Create UI(Sign In Screen)

---
+++ app/src/main/java/com/sunasterisk/appchat/ui/auth/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.sunasterisk.appchat.ui.auth
+
+import com.sunasterisk.appchat.R
+import com.sunasterisk.appchat.base.BaseActivity
+
+class SignInActivity : BaseActivity() {
+
+    override val layoutResource = R.layout.activity_sign_in
+
+    override fun initComponents() = Unit
+
+    override fun initListeners() = Unit
+}
