4f16162e0d4535fed0c5a8f0ecb026be028f568e
hieuna20168192 <nguyenanhhieu231298@gmail.com>
2020-10-26 15:34:57 +0700

    Prefs [Task][30734] Create UI(Sign Up Screen)

---
+++ app/src/main/java/com/sunasterisk/appchat/ui/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.sunasterisk.appchat.ui.auth
+
+import com.sunasterisk.appchat.R
+import com.sunasterisk.appchat.base.BaseActivity
+
+class SignUpActivity : BaseActivity() {
+
+    override val layoutResource = R.layout.activity_sign_up
+
+    override fun initComponents() = Unit
+
+    override fun initListeners() = Unit
+}
